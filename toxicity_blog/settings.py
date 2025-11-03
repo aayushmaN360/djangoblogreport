c71c095e43faeaa06394c3f715bee731d7454659
aayushman <khayuwada.aayush1234@gmail.com>
2025-11-03 22:14:16 +0545

    Update settings.py with local changes

--- toxicity_blog/settings.py
+++ toxicity_blog/settings.py
@@ -41,10 +41,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig', 
     'django.contrib.humanize',
     'ckeditor', 
-    "channels",
-    
-    
-    
+      
 ]
 
 MIDDLEWARE = [
@@ -150,7 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
